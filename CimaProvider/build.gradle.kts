0ceaa1b4115e15ed118b0f99bc2cbe034d8db3cd
zaofreefire-hub <zaofreefire@gmail.com>
2025-12-27 14:53:21 +0100

    Update build.gradle.kts

--- CimaProvider/build.gradle.kts
+++ CimaProvider/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.lagradost.cloudstream3.gradle-plugin")
-    kotlin("android")
 }
 
 cloudstream {
     language.set("ar")
     description.set("Cima provider test plugin")
     authors.set(listOf("Edigiya Store"))
+    pluginClass.set("com.cimaprovider.CimaProvider")
 }
