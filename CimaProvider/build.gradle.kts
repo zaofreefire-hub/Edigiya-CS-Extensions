0dccc2a98e12226d4b8a857e9ace8cdd130933a0
zaofreefire-hub <zaofreefire@gmail.com>
2025-12-27 13:53:34 +0100

    Create build.gradle.kts

---
+++ CimaProvider/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.lagradost.cloudstream3.plugin") version "0.0.7"
+}
+
+cloudstream {
+    authors = listOf("Edigiya Store")
+    description = "Cima provider example"
+    language = "ar"
+    manifestVersion = 1
+}
